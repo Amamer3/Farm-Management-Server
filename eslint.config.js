6accb9a4a2c5230ac1296b61b0ef6894a9a298ed
Amamer3 <tisyastephen@gmail.com>
2025-11-12 12:04:09 +0000

    Add @eslint/js as a dev dependency and update code quality checks to be non-blocking

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+const js = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
+      'no-prototype-builtins': 'off',
+      'no-useless-escape': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-console': 'off', // Allow console.log for debugging
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'coverage/', '*.config.js'],
+  },
+];
+
